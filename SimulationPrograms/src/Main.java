c6f47241e1a2fccc7728140d258f334a93f32853
Riemann01 <jonasriem@gmail.com>
2021-03-18 20:28:05 +0100

    update

--- SimulationPrograms/src/Main.java
+++ SimulationPrograms/src/Main.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
 
 public class Main {
+
+    public Reader reader = new Reader();
+
+    public Main() throws IOException {
+        reader.read();
+    }
 
     public static void main(String[] args){
         System.out.println("Hallo");
     }
+
+
 }
