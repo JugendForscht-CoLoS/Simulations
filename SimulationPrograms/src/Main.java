26621369e70030b05472e3fc1e010d12620d5c7d
tim-jaeger <66910379+tim-jaeger@users.noreply.github.com>
2021-03-22 17:12:55 +0100

    Update Main.java

--- SimulationPrograms/src/Main.java
+++ SimulationPrograms/src/Main.java
@@ -3,20 +3,21 @@ import java.io.IOException;
 
 public class Main {
 
-    public static Reader reader;
-
-    static {
-        try {
-            reader = new Reader();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        reader.read();
-        double[] location = reader.getLocation();
-
+    public static void main(String[] args) throws IOException, FileNotFoundException {
         
+        Reader[] data = new Reader[1];
+        
+        data[0] = new Reader("");
+        
+        for (Reader reader: data) {
+            
+            Writer w = new Writer("", reader);
+          
+            for (double holdingTime = 5 * 60; holdingTime < 2 * 3600; holdingTime += 5 * 60) {
+                
+                Simulator s = new Simulator(5.7, 1.01, holdingTime);
+                w.write(holdingTime, s.getDeviationOfLocation(reader);
+            }
+        }
     }
 }
