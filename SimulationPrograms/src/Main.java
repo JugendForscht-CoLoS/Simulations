267813fe0730355f3e828398cc4cbe7aa076e2fd
Riemann01 <jonasriem@gmail.com>
2021-03-27 17:00:22 +0100

    Neue Messungen

--- SimulationPrograms/src/Main.java
+++ SimulationPrograms/src/Main.java
@@ -4,24 +4,21 @@ import java.io.IOException;
 
 public class Main {
 
-    public static String repoPath = "/Users/timjaeger/Desktop/JuFo/Programme/Simulations/";
-
     public static void main(String[] args) throws IOException, FileNotFoundException {
         
         Reader[] data = new Reader[12];
         
-        File directory = new File(repoPath + "SimulationPrograms/datafiles/sunData");
+        File directory = new File("../datafiles/sunData");
         File[] files = directory.listFiles();
 
         for (int i = 0; i < files.length; i++) {
-
             data[i] = new Reader(files[i].getPath());
         }
         
         for (int i = 0; i < data.length; i++) {
 
             data[i].read();
-            Writer w = new Writer(repoPath + "SimulationPrograms/datafiles/deviations/deviation" + i + ".deviationdata", data[i]);
+            Writer w = new Writer("../datafiles/deviations/deviation" + i + ".deviationdata", data[i]);
           
             for (double holdingTime = 5 * 60; holdingTime < 3 * 3600; holdingTime += 5 * 60) {
 
